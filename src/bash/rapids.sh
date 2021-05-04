5daedf18374216ef02fc2204031677321f917ea1
Gera Shegalov <gera@apache.org>
2021-05-03 18:02:06 -0700

    jupyter-lab

--- src/bash/rapids.sh
+++ src/bash/rapids.sh
@@ -3,44 +3,56 @@ set -x
 
 SCALATEST_VERSION=3.0.5
 
+# https://stackoverflow.com/a/246128
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+
 SPARK_HOME=${SPARK_HOME:-$HOME/gits/apache/spark}
 
 # pre-requisite: `mvn package` has been run
 SPARK_RAPIDS_HOME=${SPARK_RAPIDS_HOME:-$HOME/gits/NVIDIA/spark-rapids}
 
-RAPIDS_SHELL_HOME=${RAPIDS_SHELL_HOME:-$HOME/gits/gerashegalov/rapids-shell}
-
-MODULES=${MODULES:-"sql-plugin
-	shuffle-plugin
-	shims/spark*
-	udf-compiler
-	udf-examples
-	api_validation
-	integration_tests
-	tests
-	tests-spark310+"}
-
-for module in $MODULES; do
-  RAPIDS_CLASSES=$RAPIDS_CLASSES:$SPARK_RAPIDS_HOME/$module/target/classes
-  RAPIDS_CLASSES=$RAPIDS_CLASSES:$SPARK_RAPIDS_HOME/$module/target/test-classes
-done
+RAPIDS_SHELL_HOME=${RAPIDS_SHELL_HOME:-"$(dirname $(dirname $DIR))"}
 
 SCALATEST_JARS=$(find ~/.m2 \
 	-path \*/$SCALATEST_VERSION/\* -name \*scalatest\*jar -o \
 	-path \*/$SCALATEST_VERSION/\* -name \*scalactic\*jar | tr -s "\n" ":")
 
-RAPIDS_PLUGIN_JAR=$(find $SPARK_RAPIDS_HOME -name rapids-4-spark_2.12\*SNAPSHOT.jar)
+RAPIDS_PLUGIN_JAR=$(find $SPARK_RAPIDS_HOME -regex ".*/rapids-4-spark_2.12-.*\(-SNAPSHOT\)?.jar")
 CUDF_JAR=$(find $SPARK_RAPIDS_HOME -name cudf\*jar)
-RAPIDS_CLASSPATH=$RAPIDS_CLASSES:$RAPIDS_PLUGIN_JAR:$CUDF_JAR:$SCALATEST_JARS
+RAPIDS_CLASSPATH="$RAPIDS_PLUGIN_JAR:$CUDF_JAR:$SCALATEST_JARS"
 
 SPARK_SHELL=${SPARK_SHELL:-spark-shell}
-if [[ "$SPARK_SHELL" == "spark-shell" ]]; then
-	SPARK_SHELL_RC="-I $RAPIDS_SHELL_HOME/src/scala/rapids.scala"
-elif [[ "$SPARK_SHELL" == "jupyter" ]]; then
-	export PYSPARK_DRIVER_PYTHON="$SPARK_SHELL"
-	export SPARK_SHELL="pyspark"
-	export PYSPARK_DRIVER_PYTHON_OPTS="notebook"
-fi
+
+case "$SPARK_SHELL" in
+
+	"spark-shell")
+		SPARK_SHELL_RC="-I $RAPIDS_SHELL_HOME/src/scala/rapids.scala"
+		;;
+
+	"pyspark")
+		export PYSPARK_DRIVER_PYTHON="$SPARK_SHELL"
+		;;
+
+	"jupyter")
+		export PYSPARK_DRIVER_PYTHON="$SPARK_SHELL"
+		export PYSPARK_DRIVER_PYTHON_OPTS="notebook"
+		SPARK_SHELL="pyspark"
+		;;
+
+	"jupyter-lab")
+		export PYSPARK_DRIVER_PYTHON="$SPARK_SHELL"
+		SPARK_SHELL="pyspark"
+		;;
+	*)
+		echo -n "Unknown spark-rapids driver!"
+		;;
+esac
 
 ${SPARK_HOME}/bin/${SPARK_SHELL} \
 	${SPARK_SHELL_RC} \
