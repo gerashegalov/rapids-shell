89e11acbed817a5ea1dd815858d49934a303f241
Gera Shegalov <gera@apache.org>
2021-05-04 18:00:10 -0700

    regex fix

--- src/bash/rapids.sh
+++ src/bash/rapids.sh
@@ -23,9 +23,17 @@ SCALATEST_JARS=$(find ~/.m2 \
 	-path \*/$SCALATEST_VERSION/\* -name \*scalatest\*jar -o \
 	-path \*/$SCALATEST_VERSION/\* -name \*scalactic\*jar | tr -s "\n" ":")
 
-RAPIDS_PLUGIN_JAR=$(find $SPARK_RAPIDS_HOME -regex ".*/rapids-4-spark_2.12-.*\(-SNAPSHOT\)?.jar")
+RAPIDS_PLUGIN_JAR=$(find $SPARK_RAPIDS_HOME -regex ".*/rapids-4-spark_2.12-[0-9]+\.[0-9]+\.[0-9]+\(-SNAPSHOT\)?.jar")
 CUDF_JAR=$(find $SPARK_RAPIDS_HOME -name cudf\*jar)
 RAPIDS_CLASSPATH="$RAPIDS_PLUGIN_JAR:$CUDF_JAR:$SCALATEST_JARS"
+
+FINAL_JAVA_OPTS=(
+	"-ea"
+	"-Duser.timezone=UTC"
+	"-Dlog4j.debug=true"
+	"-Dlog4j.configuration=file:${RAPIDS_SHELL_HOME}/src/conf/log4j.properties"
+	"$RAPIDS_JAVA_OPTS"
+)
 
 SPARK_SHELL=${SPARK_SHELL:-spark-shell}
 
@@ -36,7 +44,6 @@ case "$SPARK_SHELL" in
 		;;
 
 	"pyspark")
-		export PYSPARK_DRIVER_PYTHON="$SPARK_SHELL"
 		;;
 
 	"jupyter")
@@ -57,8 +64,9 @@ esac
 ${SPARK_HOME}/bin/${SPARK_SHELL} \
 	${SPARK_SHELL_RC} \
 	--driver-memory 10g \
-	--driver-java-options "-ea -Duser.timezone=UTC -Dlog4j.debug=true -Dlog4j.configuration=file:${RAPIDS_SHELL_HOME}/src/conf/log4j.properties $RAPIDS_JAVA_OPTS" \
+	--driver-java-options "${FINAL_JAVA_OPTS[*]}" \
 	--driver-class-path "$RAPIDS_CLASSPATH" \
+	--conf spark.executor.extraJavaOptions="${FINAL_JAVA_OPTS[*]}" \
 	--conf spark.executor.extraClassPath="$RAPIDS_CLASSPATH" \
 	--conf spark.plugins=com.nvidia.spark.SQLPlugin \
 	--conf spark.sql.extensions=com.nvidia.spark.rapids.SQLExecPlugin,com.nvidia.spark.udf.Plugin \
