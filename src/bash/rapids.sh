d91cd279747eb4a6e40dc7b5c5a05b76b3ff4018
Gera Shegalov <gera@apache.org>
2021-07-29 13:52:31 -0700

    reshim layer work

--- src/bash/rapids.sh
+++ src/bash/rapids.sh
@@ -12,6 +12,26 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
 
+# parse command line arguments
+while [[ $# -gt 0 ]]; do
+	key="$1"
+	case $key in
+		--n|--dry-run)
+			DRY_RUN=true
+			shift
+			;;
+
+		--help)
+			#TODO
+			shift
+			;;
+
+
+		*) #NOPE
+			;;
+	esac
+done
+
 SPARK_HOME=${SPARK_HOME:-$HOME/gits/apache/spark}
 
 # pre-requisite: `mvn package` has been run
@@ -23,16 +43,16 @@ SCALATEST_JARS=$(find ~/.m2 \
 	-path \*/$SCALATEST_VERSION/\* -name \*scalatest\*jar -o \
 	-path \*/$SCALATEST_VERSION/\* -name \*scalactic\*jar | tr -s "\n" ":")
 
-RAPIDS_PLUGIN_JAR=$(find $SPARK_RAPIDS_HOME -regex ".*/rapids-4-spark_2.12-[0-9]+\.[0-9]+\.[0-9]+\(-SNAPSHOT\)?.jar")
-CUDF_JAR=$(find $SPARK_RAPIDS_HOME -name cudf\*jar)
-RAPIDS_CLASSPATH="$RAPIDS_PLUGIN_JAR:$CUDF_JAR:$SPARK_RAPIDS_HOME/tests/target/test-classes:$SCALATEST_JARS"
+RAPIDS_PLUGIN_JAR=$(find $SPARK_RAPIDS_HOME -regex ".*/rapids-4-spark_2.12-[0-9]+\.[0-9]+\.[0-9]+\(-SNAPSHOT\)?$SHIMVER.jar")
+CUDF_JAR=$HOME/.m2/repository/ai/rapids/cudf/21.08.0-SNAPSHOT/cudf-21.08.0-SNAPSHOT-cuda11.jar
 
+RAPIDS_CLASSPATH="${RAPIDS_PLUGIN_JAR}:${CUDF_JAR}:${SPARK_RAPIDS_HOME}/tests/target/test-classes:${SCALATEST_JARS}"
+RAPIDS_JARS="file://${RAPIDS_PLUGIN_JAR},file://${CUDF_JAR}"
 FINAL_JAVA_OPTS=(
 	"-ea"
 	"-Duser.timezone=UTC"
 	"-Dlog4j.debug=true"
 	"-Dlog4j.configuration=file:${RAPIDS_SHELL_HOME}/src/conf/log4j.properties"
-	"$RAPIDS_JAVA_OPTS"
 )
 
 SPARK_SHELL=${SPARK_SHELL:-spark-shell}
@@ -47,7 +67,7 @@ export LD_LIBRARY_PATH="$CONDA_PREFIX/lib"
 case "$SPARK_SHELL" in
 
 	"spark-shell")
-		SPARK_SHELL_RC="-I $RAPIDS_SHELL_HOME/src/scala/rapids.scala"
+		# 	SPARK_SHELL_RC="-I $RAPIDS_SHELL_HOME/src/scala/rapids.scala"
 		;;
 
 	"pyspark")
@@ -78,21 +98,31 @@ else
 	GPU_FRACTION=0.96
 fi
 
-${SPARK_HOME}/bin/${SPARK_SHELL} \
-	${SPARK_SHELL_RC} \
-	--master "$LOCAL_MASTER" \
-	--driver-memory 4g \
-	--driver-java-options "${FINAL_JAVA_OPTS[*]}" \
-	--driver-class-path "$RAPIDS_CLASSPATH" \
-	--conf spark.executor.extraJavaOptions="${FINAL_JAVA_OPTS[*]}" \
-	--conf spark.executor.extraClassPath="$RAPIDS_CLASSPATH" \
-	--conf spark.plugins=com.nvidia.spark.SQLPlugin \
-	--conf spark.sql.extensions=com.nvidia.spark.rapids.SQLExecPlugin,com.nvidia.spark.udf.Plugin \
-	--conf spark.rapids.memory.gpu.minAllocFraction=0 \
-	--conf spark.rapids.memory.gpu.allocFraction="$GPU_FRACTION" \
-	--conf spark.rapids.sql.enabled=true \
-	--conf spark.rapids.sql.test.enabled=false \
-	--conf spark.rapids.sql.test.allowedNonGpu=org.apache.spark.sql.execution.LeafExecNode \
-	--conf spark.rapids.sql.explain=ALL \
-	--conf spark.rapids.sql.exec.CollectLimitExec=true \
-	"$@"
+COMMAND_ARR=(
+	${SPARK_HOME}/bin/${SPARK_SHELL}
+	${SPARK_SHELL_RC}
+	--master \"$LOCAL_MASTER\"
+	--driver-memory 4g
+	--driver-java-options \"${FINAL_JAVA_OPTS[*]} ${RAPIDS_DRIVER_OPTS}\"
+	--driver-class-path "${RAPIDS_CLASSPATH}"
+	--conf spark.executor.extraJavaOptions=\"${FINAL_JAVA_OPTS[*]} ${RAPIDS_EXEC_OPTS}\"
+	--conf spark.executor.extraClassPath=\"${RAPIDS_CLASSPATH}\"
+	--conf spark.plugins=com.nvidia.spark.SQLPlugin
+	--conf spark.sql.extensions=com.nvidia.spark.rapids.SQLExecPlugin,com.nvidia.spark.udf.Plugin
+	--conf spark.rapids.memory.gpu.minAllocFraction=0
+	--conf spark.rapids.memory.gpu.allocFraction=\"${GPU_FRACTION}\"
+	--conf spark.rapids.sql.enabled=true
+	--conf spark.rapids.sql.test.enabled=false
+	--conf spark.rapids.sql.test.allowedNonGpu=org.apache.spark.sql.execution.LeafExecNode
+	--conf spark.rapids.sql.explain=ALL
+	--conf spark.rapids.sql.exec.CollectLimitExec=true
+	\"$@\"
+)
+
+if [ "$DRY_RUN" = "true" ]; then
+	echo "# Generating SparkSubmit command to start Spark RAPIDS"
+	echo "${COMMAND_ARR[*]}"
+else
+	echo "#### Launchng generated Spark RAPIDS shell command"
+	eval "${COMMAND_ARR[*]}"
+fi
