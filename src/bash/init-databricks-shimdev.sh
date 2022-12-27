77a8327a1832646f8f740288e3ef26254913b0b6
Gera Shegalov <gera@apache.org>
2022-12-27 14:31:46 -0800

    use custom tags for git config

--- src/bash/init-databricks-shimdev.sh
+++ src/bash/init-databricks-shimdev.sh
@@ -1,21 +1,38 @@
 set -x
 
 sudo apt -y install jq zip unzip maven rsync
-sudo "$(which pip)" install pytest sre_yield requests pandas pyarrow findspark pytest-xdist pytest-order
-
 (
+  ORIG_DIR=$PWD
   cd /home/ubuntu
+
+  # Install coursier and bloop CLI
+  sudo -u ubuntu mkdir -p ./bin
+  cd ./bin
+  sudo -u ubuntu bash -c "curl -fLo cs https://github.com/coursier/launchers/raw/master/coursier && chmod +x cs && ./cs install bloop --only-prebuilt=true"
+  cd -
 
   sudo -u ubuntu git clone https://github.com/vlad2/hocon-config-printer
   cd hocon-config-printer
   sudo -u ubuntu ./install.sh
   cd -
 
-  # TODO transiton to tags to encode git config
-  # hocon-config-printer /databricks/common/conf/deploy.conf  | jq '."all-projects".spark.databricks.clusterUsageTags.clusterAllTags | fromjson | .[] | select(.key=="Creator")'
-  GIT_USER="johndoe"
-  GIT_USER_FULL_NAME="John Doe"
-  GIT_USER_EMAIL="noreply@spark-rapids-non-exisitng.org"
+  # Add custom tags to your Databricks cluster
+  # git.user.name - Github handle
+  # git.full.name - your full name
+  # git.user.email - email to use for commits
+  #
+  DBCONFJSON="/home/ubuntu/bin/hocon-config-printer /databricks/common/conf/deploy.conf"
+  GIT_USER=$($DBCONFJSON |
+    jq -r '."all-projects".spark.databricks.clusterUsageTags.clusterAllTags | fromjson | .[] | select(.key=="git.user.name") | .value'
+  )
+
+  GIT_USER_FULL_NAME=$($DBCONFJSON |
+    jq -r '."all-projects".spark.databricks.clusterUsageTags.clusterAllTags | fromjson | .[] | select(.key=="git.full.name") | .value'
+  )
+
+  GIT_USER_EMAIL=$($DBCONFJSON |
+    jq -r '."all-projects".spark.databricks.clusterUsageTags.clusterAllTags | fromjson | .[] | select(.key=="git.user.email") | .value'
+  )
 
   sudo -u ubuntu git clone https://github.com/NVIDIA/spark-rapids.git
   cd spark-rapids
@@ -26,14 +43,14 @@ sudo "$(which pip)" install pytest sre_yield requests pandas pyarrow findspark p
 
 cat << EOF >> /home/ubuntu/.bashrc
 source /usr/share/bash-completion/completions/git
-if [[ -f /home/ubuntu/.ssh/.agent-env ]]; then
-  source  /home/ubuntu/.ssh/.agent-env
+if [[ -f \$HOME/.ssh/.agent-env ]]; then
+  source  \$HOME/.ssh/.agent-env
 else
-  ssh-agent > /home/ubuntu/.ssh/.agent-env
+  ssh-agent > \$HOME/.ssh/.agent-env
 fi
 
 export SPARK_HOME=/databricks/spark
-export SPARK_CONF_DIR=$HOME
+export SPARK_CONF_DIR=\$HOME
 
-export PATH=$PATH:$HOME/bin
+export PATH=\$PATH:\$HOME/bin:\$HOME/.local/share/coursier/bin
 EOF
