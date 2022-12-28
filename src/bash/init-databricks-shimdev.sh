2720e43fe54fd261e692444d1c87ed435fb2d8d4
Gera Shegalov <gera@apache.org>
2022-12-28 15:51:39 -0800

    bugs

--- src/bash/init-databricks-shimdev.sh
+++ src/bash/init-databricks-shimdev.sh
@@ -1,45 +1,54 @@
-set -x
+set -ex
 
 sudo apt -y install jq zip unzip maven rsync
-(
-  ORIG_DIR=$PWD
-  cd /home/ubuntu
 
-  # Install coursier and bloop CLI
-  sudo -u ubuntu mkdir -p ./bin
-  cd ./bin
-  sudo -u ubuntu bash -c "curl -fLo cs https://github.com/coursier/launchers/raw/master/coursier && chmod +x cs && ./cs install bloop --only-prebuilt=true"
-  cd -
+sudo -u ubuntu mkdir -p /home/ubuntu/bin
 
-  sudo -u ubuntu git clone https://github.com/vlad2/hocon-config-printer
-  cd hocon-config-printer
-  sudo -u ubuntu ./install.sh
-  cd -
+cat << EOF >> /home/ubuntu/init-spark-rapids.sh
+#!/bin/bash
+cd /home/ubuntu
 
-  # Add custom tags to your Databricks cluster
-  # git.user.name - Github handle
-  # git.full.name - your full name
-  # git.user.email - email to use for commits
-  #
-  DBCONFJSON="/home/ubuntu/bin/hocon-config-printer /databricks/common/conf/deploy.conf"
-  GIT_USER=$($DBCONFJSON |
-    jq -r '."all-projects".spark.databricks.clusterUsageTags.clusterAllTags | fromjson | .[] | select(.key=="git.user.name") | .value'
-  )
+# Install coursier and bloop CLI
+mkdir -p ./bin
+cd ./bin
+curl -fLo cs https://github.com/coursier/launchers/raw/master/coursier
+chmod +x cs
+./cs install bloop --only-prebuilt=true
+cd -
 
-  GIT_USER_FULL_NAME=$($DBCONFJSON |
-    jq -r '."all-projects".spark.databricks.clusterUsageTags.clusterAllTags | fromjson | .[] | select(.key=="git.full.name") | .value'
-  )
+git clone https://github.com/vlad2/hocon-config-printer
+cd hocon-config-printer
+./install.sh
+cd -
 
-  GIT_USER_EMAIL=$($DBCONFJSON |
-    jq -r '."all-projects".spark.databricks.clusterUsageTags.clusterAllTags | fromjson | .[] | select(.key=="git.user.email") | .value'
-  )
-
-  sudo -u ubuntu git clone https://github.com/NVIDIA/spark-rapids.git
-  cd spark-rapids
-  sudo -u ubuntu git remote add "$GIT_USER" "git@github.com:${GIT_USER}/spark-rapids.git"
-  sudo -u ubuntu git config --global user.name "$GIT_USER_FULL_NAME"
-  sudo -u ubuntu git config --global user.email "$GIT_USER_EMAIL"
+# Add custom tags to your Databricks cluster
+# git.user.name - Github handle
+# git.full.name - your full name
+# git.user.email - email to use for commits
+#
+DBCONFJSON="/home/ubuntu/bin/hocon-config-printer /databricks/common/conf/deploy.conf"
+GIT_USER=\$(\$DBCONFJSON |
+  jq -r '."all-projects".spark.databricks.clusterUsageTags.clusterAllTags | fromjson | .[] | select(.key=="git.user.name") | .value'
 )
+
+GIT_USER_FULL_NAME=\$(\$DBCONFJSON |
+  jq -r '."all-projects".spark.databricks.clusterUsageTags.clusterAllTags | fromjson | .[] | select(.key=="git.full.name") | .value'
+)
+
+GIT_USER_EMAIL=\$(\$DBCONFJSON |
+  jq -r '."all-projects".spark.databricks.clusterUsageTags.clusterAllTags | fromjson | .[] | select(.key=="git.user.email") | .value'
+)
+
+git clone https://github.com/NVIDIA/spark-rapids.git
+cd spark-rapids
+git remote add "\$GIT_USER" "git@github.com:\${GIT_USER}/spark-rapids.git"
+git config --global user.name "\$GIT_USER_FULL_NAME"
+git config --global user.email "\$GIT_USER_EMAIL"
+EOF
+
+chmod +x /home/ubuntu/init-spark-rapids.sh
+sudo -u ubuntu /home/ubuntu/init-spark-rapids.sh
+
 
 cat << EOF >> /home/ubuntu/.bashrc
 source /usr/share/bash-completion/completions/git
